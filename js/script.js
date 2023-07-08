0a9e59ab7864894ae38248f006a389a06e6410a2
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2023-07-08 17:04:44 -0300

    Pesquisando imagens por tema

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const imagesWrapper = document.querySelector(".images");
+const loadMoreBtn = document.querySelector(".load-more");
+const searchInput = document.querySelector(".search-box input");
 
 const apiKey = "ehuyNDywyPTfCCzcSYTN3OOcGf3vkNC9aY218uFDB0joUq7E5dbOCRb5";
 const perPage = 15;
 let currentPage = 1;
+let searchTerm = null;
 
 const generateHTML = (images) => {
   imagesWrapper.innerHTML += images.map(
@@ -21,15 +24,39 @@ const generateHTML = (images) => {
 };
 
 const getImages = (apiURL) => {
+  loadMoreBtn.innerText = "Carregando...";
+  loadMoreBtn.classList.add("disabled");
   fetch(apiURL, {
     headers: { Authorization: apiKey },
   })
     .then((res) => res.json())
     .then((data) => {
       generateHTML(data.photos);
-    });
+      loadMoreBtn.innerText = "+ imagens";
+      loadMoreBtn.classList.remove("disabled");
+    }).catch(() => alert("Falha ao carregar imagens!"));
 };
+
+const loadMoreImages = () => {
+  currentPage++;
+  let apiURL = `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`;
+  apiURL = searchTerm ? `https://api.pexels.com/v1/search?query=${searchTerm}&page=${currentPage}&per_page=${perPage}` : apiURL;
+  getImages(apiURL);
+}
+
+const loadSearchImages = (e) => {
+  if(e.target.value === "") return searchTerm = null; 
+  if(e.key === "Enter") {
+    currentPage = 1;
+    searchTerm = e.target.value;
+    imagesWrapper.innerHTML = "";
+    getImages(`https://api.pexels.com/v1/search?query=${searchTerm}&page=${currentPage}&per_page=${perPage}`);
+  }
+}
+
 
 getImages(
   `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`
 );
+loadMoreBtn.addEventListener("click", loadMoreImages);
+searchInput.addEventListener("keyup", loadSearchImages);
